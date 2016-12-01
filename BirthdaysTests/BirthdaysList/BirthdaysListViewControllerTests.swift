1ab9d1d88c594443e8231ccba2a38afe6828fb04
Yohannes Wijaya <readyforservice@icloud.com>
2016-12-01 18:20:19 +0700

    Part 2: Sketches, Structure, Tests

--- BirthdaysTests/BirthdaysList/BirthdaysListViewControllerTests.swift
+++ BirthdaysTests/BirthdaysList/BirthdaysListViewControllerTests.swift
@@ -1,6 +1,6 @@
 //
-//  BirthdaysTests.swift
-//  BirthdaysTests
+//  BirthdaysListViewControllerTests.swift
+//  Birthdays
 //
 //  Created by Yohannes Wijaya on 12/1/16.
 //  Copyright © 2016 Yohannes Wijaya. All rights reserved.
@@ -9,11 +9,17 @@
 import XCTest
 @testable import Birthdays
 
-class BirthdaysTests: XCTestCase {
+class BirthdaysListViewControllerTests: XCTestCase {
+    
+    // MARK: - Stored Properties
+    
+    var viewController: BirthdaysListTableViewController!
     
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        viewController = BirthdaysListTableViewController()
     }
     
     override func tearDown() {
@@ -21,16 +27,8 @@ class BirthdaysTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testViewController_HasTableView() {
+        XCTAssertNotNil(viewController.tableView)
     }
     
 }
